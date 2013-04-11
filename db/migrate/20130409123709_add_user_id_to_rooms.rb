33f678c8aeb28e23fc66aec02c11ee6e355877d4
Ken Lu <ken@seeice.com>
2013-04-11 08:29:56 -0400

    make migration work

--- db/migrate/20130409123709_add_user_id_to_rooms.rb
+++ db/migrate/20130409123709_add_user_id_to_rooms.rb
@@ -1,11 +1,5 @@
 class AddUserIdToRooms < ActiveRecord::Migration
   def change
     add_column :rooms, :user_id, :integer
-    Room.destroy_all
-    22.times do |index|
-      Room.create(title:"room with id #{index+1}",description:"the description of room-#{index+1}")
-    end
-    User.create(username: 'admin', email: 'admin@admin.com')
-    Room.update_all({user_id: (User.first.id)}) if User.first
   end
 end
